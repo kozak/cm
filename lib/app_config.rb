77fdc68590deb6d0c53559a2d24d4f444cfac607
Krzysztof Jablonski <jablko@gmail.com>
2013-01-25 18:30:01 +0100

    App config fix

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -3,7 +3,7 @@ require 'yaml'
 env = defined?(Rails) && Rails.env
 
 path = '../../config/app_config.yml'
-path = '../../config/app_config.yml.example' unless File.exists?(path)
+path = '../../config/app_config.yml.example' unless File.exists?(File.expand_path(path, __FILE__))
 
 CONFIG = YAML.load(File.read(File.expand_path(path, __FILE__)))
 CONFIG.merge! CONFIG.fetch(env || :test, {})
